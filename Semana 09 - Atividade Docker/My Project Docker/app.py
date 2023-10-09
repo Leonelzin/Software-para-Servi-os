eb06af2f2503c21c8abe88ccee67f961704f23a2
Leonelzin <DouglasLuis2017@outlook.com.br>
2023-10-09 13:53:35 -0300

    modified:   Semana 09 - Atividade Docker/My Project Docker/Dockerfile 	modified:   Semana 09 - Atividade Docker/My Project Docker/app.py 	modified:   Semana 09 - Atividade Docker/README.md

--- Semana 09 - Atividade Docker/My Project Docker/app.py
+++ Semana 09 - Atividade Docker/My Project Docker/app.py
@@ -1,1 +1,10 @@
-print("Olá, Docker!")
+import http.server
+import socketserver
+
+PORT = 8080
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    httpd.serve_forever()
