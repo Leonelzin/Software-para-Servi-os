053ed96f3d77a22231efac01c1d5b0a187ced11b
Leonelzin <DouglasLuis2017@outlook.com.br>
2023-10-09 14:11:13 -0300

    modified:   Semana 09 - Atividade Docker/My Project Docker/app.py

--- Semana 09 - Atividade Docker/My Project Docker/app.py
+++ Semana 09 - Atividade Docker/My Project Docker/app.py
@@ -1,5 +1,6 @@
 import http.server
 import socketserver
+from flask import Flask
 
 PORT = 8080
 
@@ -8,3 +9,12 @@ Handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at http://localhost:{PORT}")
     httpd.serve_forever()
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!\n"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
