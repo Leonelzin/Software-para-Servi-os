da80b0c069f115f4af67fbcd1ddb0985137347b2
Leonelzin <DouglasLuis2017@outlook.com.br>
2023-10-19 20:41:35 -0300

    new file:   .vscode/settings.json 	renamed:    "Semana 10 - Atividade Pilha de Desenvolvimento Web Condi\303\247\303\265es de conclus\303\243o/Dockerfile" -> Semana 10 - Atividade Pilha/Dockerfile 	renamed:    "Semana 10 - Atividade Pilha de Desenvolvimento Web Condi\303\247\303\265es de conclus\303\243o/dockerignore.txt" -> Semana 10 - Atividade Pilha/dockerignore.txt 	new file:   Semana 10 - Atividade Pilha/index.php

---
+++ Semana 10 - Atividade Pilha/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Meu Aplicativo PHP</title>
+</head>
+<body>
+    <h1>Olá, Mundo!</h1>
+    <p>Este é um exemplo simples de um aplicativo PHP em execução no Docker.</p>
+    <p>A versão do PHP é <?php echo phpversion(); ?></p>
+</body>
+</html>
